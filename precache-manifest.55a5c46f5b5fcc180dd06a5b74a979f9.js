359420f57af2f0cb02c97c96a38b00afd90493bf
Vladimir Alekseychuk <alekseychuk972mail.ru@gmail.com>
2019-09-22 19:07:45 +0300

    Updates

---
+++ precache-manifest.55a5c46f5b5fcc180dd06a5b74a979f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be3a85dd23b63cdc90a1095210f467f8",
+    "url": "/goit-react-hw-04-reader/index.html"
+  },
+  {
+    "revision": "daeaf80a73323328426c",
+    "url": "/goit-react-hw-04-reader/static/css/main.f758b9c2.chunk.css"
+  },
+  {
+    "revision": "328d7a7c7614f3068efa",
+    "url": "/goit-react-hw-04-reader/static/js/2.a1dcf02d.chunk.js"
+  },
+  {
+    "revision": "daeaf80a73323328426c",
+    "url": "/goit-react-hw-04-reader/static/js/main.38443235.chunk.js"
+  },
+  {
+    "revision": "717ffc1dda4ed3ad151c",
+    "url": "/goit-react-hw-04-reader/static/js/runtime~main.51d728b5.js"
+  }
+]);
